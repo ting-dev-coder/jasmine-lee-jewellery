86c0b32886555f403c0ceeef99a924bc464e66d1
ting-dev-coder <missjoker59@gmail.com>
2025-03-15 22:48:23 -0700

    Add responsive menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,10 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header style="height: 100px" class="q-py-md bg-grey">
-      <div class="container flex justify-between align-items full-height">
+      <q-toolbar class="container flex justify-between align-items full-height">
         <h1 class="full-height"><img style="height: 100%" src="logo.png" /></h1>
-
-        <ul class="self-center text-dark">
+        <q-btn class="lt-md" flat @click="drawerLeft = !drawerLeft" round dense icon="menu" />
+        <ul class="gt-sm self-center text-dark">
           <q-btn
             class="q-ml-sm"
             dense
@@ -15,8 +15,30 @@
             >{{ item.label }}</q-btn
           >
         </ul>
-      </div>
+      </q-toolbar>
     </q-header>
+
+    <q-drawer
+      v-model="drawerLeft"
+      :width="200"
+      :breakpoint="1200"
+      elevated
+      class="bg-primary text-black"
+    >
+      <q-scroll-area class="fit">
+        <ul class="self-center text-dark">
+          <q-btn
+            class="block"
+            align="left"
+            flat
+            v-for="(item, idx) in menu"
+            :key="`menu-${idx}`"
+            :to="item.path"
+            >{{ item.label }}</q-btn
+          >
+        </ul>
+      </q-scroll-area>
+    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -27,7 +49,9 @@
 
 <script setup>
 import TheFooter from '@components/TheFooter'
+import { ref } from 'vue'
 
+const drawerLeft = ref(false)
 const menu = [
   {
     label: 'About',
