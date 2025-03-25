c9700568aa7d680405a3119bde7029b746471af9
ting-dev-coder <missjoker59@gmail.com>
2025-03-25 15:12:01 -0700

    Modify public path, add ssr config

--- quasar.config.js
+++ quasar.config.js
@@ -45,7 +45,7 @@ export default defineConfig((/* ctx */) => {
       // vueOptionsAPI: false,
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
-      publicPath: '/jasmine-lee-jewelry/',
+      publicPath: '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
