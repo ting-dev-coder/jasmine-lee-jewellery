b191a4f52ac5295c5ba68b2d33bf64dec1431a2f
ting-dev-coder <missjoker59@gmail.com>
2025-03-25 19:33:01 -0700

    Updates

---
+++ assets/ssg-CCE5QbD9.js
@@ -0,0 +1,1 @@
+import{Q as a}from"./index-BXTPs8Qm.js";const{ssrUpdate:t}=a,r=({app:s})=>{t({app:s})};export{r as default};
---
+++ assets/use-timeout-D9Twu36a.js
@@ -0,0 +1,1 @@
+import{v as u}from"./use-router-link-BB50CFaT.js";import{B as i,b as m,g as s}from"./index-BXTPs8Qm.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
