15c735c6b8b6a0aed7afdf70832e426e0ac23589
ting-dev-coder <missjoker59@gmail.com>
2025-03-25 19:24:16 -0700

    Updates

--- client/assets/use-timeout-C0qhP15V.js
+++
@@ -1,1 +0,0 @@
-import{v as u}from"./use-router-link-81J_ot6o.js";import{B as i,b as m,g as s}from"./index-BdlPve62.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
