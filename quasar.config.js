bbfbf962237c1ecd3986077f9a21ecb63eb3300a
ting-dev-coder <missjoker59@gmail.com>
2025-03-25 17:52:06 -0700

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -1,7 +1,7 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file
 
-import { defineConfig } from '#q-app/wrappers'
+import { defineConfig } from '@quasar/cli'
 import path from 'path'
 
 export default defineConfig((/* ctx */) => {
